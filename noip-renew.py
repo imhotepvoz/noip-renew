77466664b6e85c4746937b53c8c735e3e33e12ca
imhotepvoz <imhotep.voz@gmail.com>
2025-06-26 16:15:02 +0700

    Update noip-renew.py

--- noip-renew.py
+++ noip-renew.py
@@ -120,7 +120,7 @@ class Robot:
         # After Loggin browser loads my.noip.com page - give him some time to load
         # 'noip-cart' element is near the end of html, so html have been loaded
         try:
-            elem = WebDriverWait(self.browser, 10).until( EC.presence_of_element_located((By.ID, "noip-cart")))
+            elem = WebDriverWait(self.browser, 30).until( EC.presence_of_element_located((By.ID, "noip-cart")))
         except:
             raise Exception("my.noip.com page could not load")        
 
